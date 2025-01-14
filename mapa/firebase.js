d119bbbc18b97e4a74800e62d7d072b121ca4f72
jadersantana <74872530+jadersantana@users.noreply.github.com>
2025-01-14 10:41:36 -0300

    revert

--- mapa/firebase.js
+++ mapa/firebase.js
@@ -2,8 +2,7 @@
 
 // Carregamento de scripts do Firebase
 
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
+
 
 // Configuração do Firebase
 const firebaseConfig = {
