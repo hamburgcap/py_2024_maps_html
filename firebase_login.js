6348055d9d585bd513c2f00889d2bba847f8e958
jadersantana <74872530+jadersantana@users.noreply.github.com>
2025-01-17 14:50:02 -0300

    return firebase

--- firebase_login.js
+++ firebase_login.js
@@ -1,19 +1,1 @@
-// Configuração do Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyAwVI2G9I_2ThVcHPCFiqVQfYNYZvfPgvg",
-    authDomain: "rotadosleiloes.firebaseapp.com",
-    projectId: "rotadosleiloes",
-    storageBucket: "rotadosleiloes.firebasestorage.app",
-    messagingSenderId: "1022316924185",
-    appId: "1:1022316924185:web:72194a4fe18508c104c988",
-    measurementId: "G-D5NQPEWTZQ"
-};
 
-// Inicializa o Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Inicializa o serviço de autenticação
-const auth = firebase.auth();
-
-// Exporta o objeto `auth` para ser usado em outros arquivos
-export { auth };
